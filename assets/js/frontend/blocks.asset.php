b2f89027d0117ca00dc5871b25b5515ee1290778
Hazhe-Chakamaraq <165942489+Hazhe-Chakamaraq@users.noreply.github.com>
2024-07-02 14:02:44 +0300

    resolving conflicts

--- assets/js/frontend/blocks.asset.php
+++ assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '74fe7fbdbc631ed52655');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'a9db78a1b4c31a2d291b');
